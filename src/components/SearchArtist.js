147a74bc3450710c325a2400f397cf66b2959d61
Manor, Yanir (ym903w) <ym903w@intl.att.com>
2021-03-08 09:15:16 +0200

    [Fix] appbar title

--- src/components/SearchArtist.js
+++ src/components/SearchArtist.js
@@ -6,7 +6,6 @@ import { favorite } from "../Store";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
@@ -113,9 +112,7 @@ export default function SearchArtist() {
     <div className="container mx-auto">
       <AppBar position="sticky">
         <Toolbar>
-          <Typography variant="h6" noWrap>
-            Who’s In Town
-          </Typography>
+          <div className="text-center">Who’s In Town</div>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
